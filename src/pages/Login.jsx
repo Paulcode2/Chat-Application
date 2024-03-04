1a998ce1bf8fa2908502efda8643009cffbfebba
paul <paullevites84@gmail.com>
2024-03-04 12:12:56 +0100

    fixed a bug

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -2,18 +2,18 @@ import React from 'react'
 
 const Login = () => {
   return (
-    // <div className='register'>
-    //   <div className="form">
-    //     <span id="logo">Chat Application</span>
-    //     <span id="title">Login</span>
-    //     <form action="">
-    //         <input type="text" placeholder='Username'/>
-    //         <input type="password" placeholder='password'/>
-    //         <button>Sign in</button>
-    //     </form>
-    //     <p>Already an account? Register</p>
-    //   </div>
-    // </div>
+    <div className='register'>
+      <div className="form">
+        <span id="logo">Chat Application</span>
+        <span id="title">Login</span>
+        <form action="">
+            <input type="text" placeholder='Username'/>
+            <input type="password" placeholder='password'/>
+            <button>Sign in</button>
+        </form>
+        <p>Already an account? Register</p>
+      </div>
+    </div>
   )
 }
 
